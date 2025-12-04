417d8e67350457b5afb4b4e3bbcda4708baa55e1
rabih0 <rabih.alahmad@icloud.com>
2025-12-04 03:06:41 +0100

    W

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: process.env.NODE_ENV === 'production' ? '/FAKTUR-FIX/' : '/',
+    base: process.env.NODE_ENV === 'production' ? '/faktur-fix/' : '/',
     server: {
       port: 3000,
       host: '0.0.0.0',
